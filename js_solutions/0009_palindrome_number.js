eb3c1eed08e7ce4fe383be092c3bbc15884f89c2
alex_p <aleksborislavskij@gmail.com>
2022-09-20 17:36:57 +0300

    add solution in JS

---
+++ js_solutions/0009_palindrome_number.js
@@ -0,0 +1,15 @@
+/**
+ * @link https://leetcode.com/problems/palindrome-number/
+ * @description Check if given number is a palindrome
+ * @param {Number} number 
+ * @returns {Boolean}
+ */
+const isPalindrome = (number) => {
+  const convertedToString = number.toString();
+  for (let i = 0, j = convertedToString.length - 1; i < j; i += 1, j -= 1) {
+    if (string[i] !== string[j]) {
+      return false;
+    }
+  }
+  return true;
+};

2b619907700d018e8affb505ae937b55f2d79555
alex_p <aleksborislavskij@gmail.com>
2022-09-22 00:29:38 +0300

    add solution in JS for 0412_fizz_buzz.js

--- LeetCode/Arrays.js
+++ LeetCode/Arrays.js
@@ -1,56 +1,25 @@
-
-// Нужно найти префикс / Z функцию
-
-// const z_function_advanced = (s) => {
-//   const z = new Array(s.length).fill(0);
-//   let left = 0;
-//   let right = 0;
-//   for (let i = 1; i < s.length; i += 1) {
-//     // ПЕРВЫЙ СЛУЧАЙ 
-//     if (i <= right) {
-//       // ЕСЛИ ИНДЕКС ПРИНАДЛЕЖИТ САМОМУ ПРАВОМУ ОТРЕЗКУ СОВПАДЕНИЯ, ТО ВЫЬИРАЕМ МИНИМАЛЬНОЕ
-//       z[i] = Math.min(right - i + 1, z[i - left])
-//     }
-//     while (i + z[i] < s.length && s[z[i]] === s[i + z[i]]) {
-//       z[i] += 1;
-//     }
-//     if (i + z[i] - 1 > right) {
-//       left = i;
-//       right = i + z[i] - 1;
-//     }
-//   }
-//   return z;
-// };
-
-// // Прописываем КМП
-
-// const strStr = (string, substring) => {
-//   const separator = '#';
-//   const zFunction = z_function_advanced(`${substring}${separator}${string}`);
-//   for(let i = 0; i < zFunction.length; i += 1) {
-//     if(zFunction[i] === substring.length) {
-//       return i - substring.length - 1;
-//     }
-//   }
-//   return -1;
-// };
-
-// const haystack = 'hellodarling';
-// const needle = 'odar'; 
-
-// console.log(strStr(haystack, needle));
-
-const haystack = 'leetcode';
-const needle = 'leeto'; 
-
-const findFirstIndex = (string, substring) => {
-  for(let i = 0; i < string.length; i += 1) {
-    if(string.slice(i, substring.length + i) === substring) {
-      return i;
+/**
+ * @link https://leetcode.com/problems/fizz-buzz/submissions/
+ * @description Given an integer n, return a string array answer (1-indexed) where:
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * answer[i] == "Fizz" if i is divisible by 3.
+ * answer[i] == "Buzz" if i is divisible by 5.
+ * answer[i] == i (as a string) if none of the above conditions are true.
+ * @param {Number} n 
+ * @returns {Object}
+ */
+const fizzBuzz = (n) => {
+  const result = [];
+  for (let i = 1; i <= n; i += 1) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      result.push(i);
     }
   }
-  return -1;
+  return result;
 };
-
-console.log(findFirstIndex(haystack, needle));
-

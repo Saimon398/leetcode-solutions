2bfd17fae598197e111cc59c8d53f549e6f9ba23
alex_p <aleksborislavskij@gmail.com>
2022-09-21 23:05:57 +0300

    add solution in JS for 0028_find_the_index_of_the_first_occurence.js

--- LeetCode/Arrays.js
+++ LeetCode/Arrays.js
@@ -1,25 +1,56 @@
-/**
- * @link https://leetcode.com/problems/sqrtx/submissions/
- * @description Return the square root of x.
- * @param {Number} number 
- * @returns {Number}
- */
-const mySqrt = (number) => {
-  let ans = 0;
-  let sqrt = 0;
-  let e = number;
-  while (sqrt <= e) {
-    const middle = Math.trunc((sqrt + e) / 2);
-    if (middle * middle === number) {
-      return middle;
-    }
-    else if (middle * middle < number)  {
-      sqrt = middle + 1;
-      ans = middle;
-    }
-    else {
-      e = middle - 1;
+
+// Нужно найти префикс / Z функцию
+
+// const z_function_advanced = (s) => {
+//   const z = new Array(s.length).fill(0);
+//   let left = 0;
+//   let right = 0;
+//   for (let i = 1; i < s.length; i += 1) {
+//     // ПЕРВЫЙ СЛУЧАЙ 
+//     if (i <= right) {
+//       // ЕСЛИ ИНДЕКС ПРИНАДЛЕЖИТ САМОМУ ПРАВОМУ ОТРЕЗКУ СОВПАДЕНИЯ, ТО ВЫЬИРАЕМ МИНИМАЛЬНОЕ
+//       z[i] = Math.min(right - i + 1, z[i - left])
+//     }
+//     while (i + z[i] < s.length && s[z[i]] === s[i + z[i]]) {
+//       z[i] += 1;
+//     }
+//     if (i + z[i] - 1 > right) {
+//       left = i;
+//       right = i + z[i] - 1;
+//     }
+//   }
+//   return z;
+// };
+
+// // Прописываем КМП
+
+// const strStr = (string, substring) => {
+//   const separator = '#';
+//   const zFunction = z_function_advanced(`${substring}${separator}${string}`);
+//   for(let i = 0; i < zFunction.length; i += 1) {
+//     if(zFunction[i] === substring.length) {
+//       return i - substring.length - 1;
+//     }
+//   }
+//   return -1;
+// };
+
+// const haystack = 'hellodarling';
+// const needle = 'odar'; 
+
+// console.log(strStr(haystack, needle));
+
+const haystack = 'leetcode';
+const needle = 'leeto'; 
+
+const findFirstIndex = (string, substring) => {
+  for(let i = 0; i < string.length; i += 1) {
+    if(string.slice(i, substring.length + i) === substring) {
+      return i;
     }
   }
-  return ans;
+  return -1;
 };
+
+console.log(findFirstIndex(haystack, needle));
+
---
+++ js_solutions/0028_find_the_index_of_the_first_occurrence.js
@@ -0,0 +1,15 @@
+/**
+ * @link https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+ * @description return the index of the first occurrence of needle in haystack
+ * @param {String} haystack 
+ * @param {String} needle
+ * @returns 
+ */
+const strStr = (haystack, needle) => {
+  for(let i = 0; i < haystack.length; i += 1) {
+    if(haystack.slice(i, needle.length + i) === needle) {
+      return i;
+    }
+  }
+  return -1;
+};

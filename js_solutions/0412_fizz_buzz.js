2db91d81734c9729a76d82c347ad4b09acc710fe
alex_p <aleksborislavskij@gmail.com>
2022-09-22 00:31:06 +0300

    add solution in JS for 0412-fizz_buzz.js

---
+++ js_solutions/0412_fizz_buzz.js
@@ -0,0 +1,25 @@
+/**
+ * @link https://leetcode.com/problems/fizz-buzz/submissions/
+ * @description Given an integer n, return a string array answer (1-indexed) where:
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * answer[i] == "Fizz" if i is divisible by 3.
+ * answer[i] == "Buzz" if i is divisible by 5.
+ * answer[i] == i (as a string) if none of the above conditions are true.
+ * @param {Number} n 
+ * @returns {Object}
+ */
+ const fizzBuzz = (n) => {
+  const result = [];
+  for (let i = 1; i <= n; i += 1) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+};

a86fa1a1399f2850f9cbd8afac6d24ef5a5101b5
alex_p <aleksborislavskij@gmail.com>
2022-09-22 14:13:50 +0300

    add solution in JS for 1143_longest_common_subsequence.js

--- LeetCode/Arrays.js
+++ LeetCode/Arrays.js
@@ -1,28 +1,32 @@
-const isSubsequence = (sub, target) => {
-  // If strings are the same length, they must be identical
-  if(sub.length === target.length) {
-    return sub === target;
-  }
-  // If string is longer that target, it cannot be a subsequence
-  else if (sub.length > target.length) {
-    return false;
-  }
-
-  // i: subsequence index, j: target index
-  let i = 0;
-  let j = 0;
-
-  while (i < sub.length && j < target.length) {
-    const sub_char = sub[i];
-    const target_char = target[j];
-    if(sub_char === target_char) {
-      i += 1;
-      j += 1;
+function generate_matrix(rows, columns, filler) {
+  const matrix = [];
+  for (let i = 0; i < rows; i += 1) {
+    const submatrix = [];
+    for (let k = 0; k < columns; k += 1) {
+      submatrix[k] = filler;
     }
-    else {
-      j += 1;
-    }
+    matrix[i] = submatrix;
   }
-  return i >= sub.length;
+  return matrix;
 };
 
+const longestCommonSubsequence = (A, B) => {
+  const F = generate_matrix(A.length + 1, B.length + 1, 0);
+  // Заполняем слева направо
+  for (let i = 1; i < A.length + 1; i += 1) {
+    for (let j = 1; j < B.length + 1; j += 1) {
+      //  СРАВНИВАЕМ 2 ПОСЛЕДНИХ ЭЛЕМЕНТА A[i] и B[j], но они находятся под индексами i - 1, j - 1
+      if (A[i - 1] === B[j - 1]) {
+        F[i][j] = 1 + F[i - 1][j - 1];
+      }
+      else {
+        F[i][j] = Math.max(F[i - 1][j], F[i][j - 1]);
+      }
+    }
+  }
+  return F[A.length][B.length];
+};
+
+const text1 = "abc";
+const text2 = 'def';
+console.log(largest_common_subsequence(text1, text2));

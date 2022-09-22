548d425f969b7316dbfca9e9f02700a5898959fe
alex_p <aleksborislavskij@gmail.com>
2022-09-22 14:48:40 +0300

    add solution in JS for 1207_unique_numbers_of_occurrences.js

--- LeetCode/Arrays.js
+++ LeetCode/Arrays.js
@@ -1,32 +1,11 @@
-function generate_matrix(rows, columns, filler) {
-  const matrix = [];
-  for (let i = 0; i < rows; i += 1) {
-    const submatrix = [];
-    for (let k = 0; k < columns; k += 1) {
-      submatrix[k] = filler;
-    }
-    matrix[i] = submatrix;
-  }
-  return matrix;
+
+const uniqueOccurrences = (collection) => {
+  const hashMap = collection
+    .reduce((acc, item) => {
+      acc[item] = (acc[item] + 1) || 1;
+      return { ...acc };
+    }, {});
+  const set = new Set(Object.values(hashMap));
+  return set.size === Object.values(hashMap).length;
 };
 
-const longestCommonSubsequence = (A, B) => {
-  const F = generate_matrix(A.length + 1, B.length + 1, 0);
-  // Заполняем слева направо
-  for (let i = 1; i < A.length + 1; i += 1) {
-    for (let j = 1; j < B.length + 1; j += 1) {
-      //  СРАВНИВАЕМ 2 ПОСЛЕДНИХ ЭЛЕМЕНТА A[i] и B[j], но они находятся под индексами i - 1, j - 1
-      if (A[i - 1] === B[j - 1]) {
-        F[i][j] = 1 + F[i - 1][j - 1];
-      }
-      else {
-        F[i][j] = Math.max(F[i - 1][j], F[i][j - 1]);
-      }
-    }
-  }
-  return F[A.length][B.length];
-};
-
-const text1 = "abc";
-const text2 = 'def';
-console.log(largest_common_subsequence(text1, text2));
---
+++ js_solutions/1207_unique_number_of_occurrences.js
@@ -0,0 +1,20 @@
+/**
+ * @link https://leetcode.com/problems/unique-number-of-occurrences/
+ * @description  return true if the number of occurrences of each value 
+ * in the array is unique, or false otherwise.
+ * @param {Object []} collection 
+ * @returns {Boolean}
+ */
+const uniqueOccurrences = (collection) => {
+  // Build hash map, where the values of collection items
+  // are number of occurences
+  const hashMap = collection
+    .reduce((acc, item) => {
+      acc[item] = (acc[item] + 1) || 1;
+      return { ...acc };
+    }, {});
+  // Declare set b/c it contains only unique values
+  const set = new Set(Object.values(hashMap));
+  // Compare sizes of set and array with the same values
+  return set.size === Object.values(hashMap).length;
+};

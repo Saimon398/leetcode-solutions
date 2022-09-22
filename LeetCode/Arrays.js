89b81b67215fbefbf906234c7b422e0ee298f0dd
alex_p <aleksborislavskij@gmail.com>
2022-09-22 10:38:03 +0300

    add solution in JS for 0151_reverse_words_in_a_string.js

--- LeetCode/Arrays.js
+++ LeetCode/Arrays.js
@@ -1,25 +1,14 @@
-/**
- * @link https://leetcode.com/problems/fizz-buzz/submissions/
- * @description Given an integer n, return a string array answer (1-indexed) where:
- * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
- * answer[i] == "Fizz" if i is divisible by 3.
- * answer[i] == "Buzz" if i is divisible by 5.
- * answer[i] == i (as a string) if none of the above conditions are true.
- * @param {Number} n 
- * @returns {Object}
- */
-const fizzBuzz = (n) => {
-  const result = [];
-  for (let i = 1; i <= n; i += 1) {
-    if(i % 3 === 0 && i % 5 === 0) {
-      result.push('FizzBuzz');
-    } else if (i % 3 === 0) {
-      result.push('Fizz');
-    } else if (i % 5 === 0) {
-      result.push('Buzz');
-    } else {
-      result.push(i);
+const reverseWords = (string) => {
+  const words = string.split(' ');
+  const normalizedWords = [];
+  for (let i = words.length - 1; i >= 0; i -= 1) {
+    if(words[i] !== '') {
+      normalizedWords.push(words[i].trim());
     }
   }
-  return result;
+  return normalizedWords.join(' ');
 };
+
+const s =  "  hello world  ";
+
+console.log(reverseWords(s));
---
+++ js_solutions/0151_reverse_words_in_a_string.js
@@ -0,0 +1,20 @@
+/**
+ * @link https://leetcode.com/problems/reverse-words-in-a-string/
+ * @description reverse the order of the words.
+ * @param {String} string 
+ * @returns {String}
+ */
+const reverseWords = (string) => {
+  // Split string into words
+  const words = string.split(' ');
+  const normalizedWords = [];
+
+  // Iterate through all words and in reverse order
+  // and remove spaces in each word
+  for (let i = words.length - 1; i >= 0; i -= 1) {
+    if(words[i] !== '') {
+      normalizedWords.push(words[i].trim());
+    }
+  }
+  return normalizedWords.join(' ');
+};

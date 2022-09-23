2ad42ac74bd07590c75d6393e6f93ffac1baae47
alex_p <aleksborislavskij@gmail.com>
2022-09-23 12:16:39 +0300

    add solution in JS for 0075_sort_colors.js

--- js_solutions/0075_sort_colors.js
+++ js_solutions/0075_sort_colors.js
@@ -1,14 +1,25 @@
-const colors = [2,0,2,1,1,0];
-
+/**
+ * @link https://leetcode.com/problems/sort-colors/
+ * @description Objects colored red, white, or blue, sort them in-place 
+ * so that objects of the same color are adjacent
+ * @param {Object []} colors 
+ * 
+ * Solution is based on quick sort algorithm
+ */
 const sortColors = (colors) => {
+  // Terminal Case 
   if(colors.length <= 1) {
     return;
   }
+  // Select pivot item in array 
   const [pivot] = colors;
   const smallerThan = [];
   const equal = [];
   const greaterThan = [];
 
+  // Iterate through all colors and compare 
+  // the rest to him, allocating them 
+  // according to their value
   for (let i = 0; i < colors.length; i += 1) {
     const color = colors[i];
     if (color < pivot) {
@@ -21,10 +32,12 @@ const sortColors = (colors) => {
       greaterThan.push(color);
     }
   }
-
+  // Launch sort algo for arrays with 
+  // smaller and greater values
   sortColors(smallerThan);
   sortColors(greaterThan);
 
+  // Swap items in the original array
   let i = 0;
   for(const color of [...smallerThan, ...equal, ...greaterThan]) {
     colors[i] = color;

3328b08e908b59058b5f352e6cef4f0397420be5
alex_p <aleksborislavskij@gmail.com>
2022-11-28 01:41:59 +0300

    add solution in JS for 0231_power_of_two.js

---
+++ js_solutions/0231_power_of_two.js
@@ -0,0 +1,15 @@
+/**
+ * @link https://leetcode.com/problems/power-of-two/
+ * @description Return true if num is a power of two. Otherwise, return false.
+ * @param {Number} num 
+ * @returns {Boolean}
+ * @example
+ * isPowerOfTwo(16); // => true
+ * 
+ * Explanation:
+ * num = 16 = 10000
+ * num - 1 = 15 = 01111
+ * num & (num - 1) = 10000 AND 01111 => 0
+ * 
+ */
+const isPowerOfTwo = (num) => num > 0 && (num & num - 1) === 0;

052a9035a0f5506c4161477e0a3936f512c94b03
alex_p <aleksborislavskij@gmail.com>
2022-11-28 12:24:27 +0300

    add solution in JS for 0263_ugly_number.js

---
+++ js_solutions/0263_ugly_number.js
@@ -0,0 +1,18 @@
+/**
+ * @link https://leetcode.com/problems/ugly-number/
+ * @description Given an integer num, return true if num is an ugly number.
+ * @param {Number} num
+ * @returns {Boolean}
+ */
+const isUgly = (num) => {
+  if (num === 0) return false;
+  const limits = [2, 3, 5];
+  // Iterate through the limits and check if
+  // each of them is a prime factor for num
+  for (let i = 0; i < limits.length; i += 1) {
+    while (num % limits[i] === 0) {
+      num / limits[i];
+    }
+  }
+  return num === 1;
+};

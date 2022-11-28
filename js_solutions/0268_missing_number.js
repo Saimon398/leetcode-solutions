720118c3fadd6d3ccaccb81449b9d4ca1c104906
alex_p <aleksborislavskij@gmail.com>
2022-11-28 22:50:14 +0300

    add solution in JS for 0268_missing_number.js

---
+++ js_solutions/0268_missing_number.js
@@ -0,0 +1,14 @@
+/**
+ * @link https://leetcode.com/problems/missing-number/
+ * @description Return the only number in the range
+ * that is missing from the array.
+ * @param {Array []} nums 
+ * @returns {Number}
+ */
+const missingNumber = (nums) => {
+  let result = 0;
+  for (let i = 0; i < nums.length; i += 1) {
+    result = result ^ i ^ nums[i];
+  }
+  return result ^ nums.length;
+};

6bcc6866ba8e2cdd1798ba1559a7d547ac0f1162
alex_p <aleksborislavskij@gmail.com>
2022-11-29 11:48:44 +0300

    add solution in JS for 0451_sort_characters_by_frequency.js

---
+++ js_solutions/0451_sort_characters_by_frequency.js
@@ -0,0 +1,22 @@
+/**
+ * @link https://leetcode.com/problems/sort-characters-by-frequency/
+ * @description Return the sorted string according to the frequencies of chars
+ * appeared in the given string
+ * @param {String} str String to be sorted
+ * @returns {String}
+ */
+const frequencySort = (str) => {
+  // Find array of frequencies of chars in the string
+  const frequency = str.split('')
+    .reduce((acc, char) => {
+      acc[char] = (acc[char] || 0) + 1;
+      return { ...acc };
+    }, {});
+  // Sort chars according to their frequencies and convert them into a string
+  return Object.entries(frequency)
+    .sort(([, value1], [, value2]) => value2 - value1)
+    .reduce((acc, [char, frequency]) => {
+      acc += char.repeat(frequency);
+      return acc;
+    }, '')
+};
